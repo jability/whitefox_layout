b1e76ef69f936376c8b2eddb8de12b06e7366fff
Julien LINDEN <jlinden@student.42.fr>
2017-07-29 18:58:46 +0200

    Fix MT_CTMUX Macro

--- keymap.c
+++ keymap.c
@@ -9,11 +9,7 @@
 /* MACROS */
 #define M_TMUX 0
 
-#define MACRO_CTRL_TMUX MACROTAP(M_TMUX)
-
-enum {
-	K_ESC = 0,
-};
+#define MT_CTMUX MACROTAP(M_TMUX)
 
 /* LAYERS */
 enum {
@@ -41,7 +37,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_MAIN] = KEYMAP( \
 	       KC_GESC,      KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,   KC_MINS,    KC_EQL,   KC_BSLS,    KC_GRV,TG(L_GAME), \
             KC_TAB,      KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,   KC_LBRC,   KC_RBRC,   KC_BSPC,               KC_DEL, \
-            M_TMUX,      KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,   KC_SCLN,   KC_QUOT,   KC_NUHS,               KC_ENT,   KC_PGUP, \
+          MT_CTMUX,      KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,   KC_SCLN,   KC_QUOT,   KC_NUHS,               KC_ENT,   KC_PGUP, \
            KC_LSPO,   KC_NUBS,      KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,              KC_RSPC,     KC_UP,   KC_PGDN, \
 		 MO(L_FCT),   KC_LALT,   KC_LGUI,                                                          LT(L_FCT, KC_SPC),   KC_CAPS, MO(L_ADV),    XXXXXX,   KC_LEFT,   KC_DOWN,   KC_RGHT \
     ),
