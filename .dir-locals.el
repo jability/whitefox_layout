366c19de2a68d88782a8fe942a473450e30b5bba
Julien LINDEN <jlinden@student.42.fr>
2017-07-29 14:09:23 +0200

    Add dir-locals emacs file

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+
+((c-mode
+  (flycheck-clang-include-path . (
+								  "../.."
+								  "../../../../quantum"
+								  "../../../../quantum/audio"
+								  "../../../../quantum/process_keycode"
+								  "../../../../tmk_core/common"
+								  ))))
