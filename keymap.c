79a6f9ef043ea96bee408ef68ca645f94ffd4a74
Julien LINDEN <jlinden@student.42.fr>
2017-08-04 19:45:00 +0200

    Multiple changes to layers and macros

--- keymap.c
+++ keymap.c
@@ -5,9 +5,10 @@
 #define ______ KC_TRNS
 #define XXXXXX KC_NO
 #define CA(kc) LCTL(LALT(kc))
+#define CS(kc) LCTL(S(kc))
 enum {
-	KC_BCBO = 0,	/* Brackets and Curly Braces Open */
-	KC_BCBC			/* Brackets and Curly Braces Close */
+    KC_BCBO = 0,    /* Brackets and Curly Braces Open */
+    KC_BCBC         /* Brackets and Curly Braces Close */
 };
 
 /* MACROS */
@@ -17,15 +18,16 @@ enum {
 
 /* LAYERS */
 enum {
-	L_MAIN = 0,
-	L_FCT,
-	L_ADV,
-	L_GAME
+    L_MAIN = 0,     /* Basic Layer */
+    L_SFN,          /* SpaceFn Layer */
+    L_SYS,          /* System Layer */
+    L_OSM,          /* One-Shot Macros Layer */
+    L_GAME          /* Gaming Layer */
 };
 
 /* KEYMAPS */
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Layer 0: Default Layer
+    /* Basic Layer
      * ,---------------------------------------------------------------.
      * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|GAM|
      * |---------------------------------------------------------------|
@@ -35,17 +37,17 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |---------------------------------------------------------------|
      * |  LSPO  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RSPC |Up |PgD|
      * |---------------------------------------------------------------|
-     * | Fn |Alt |Gui |        SpaceFn       |Caps |ADV |  |Lef|Dow|Rig|
+     * | Fn |Alt |Gui |        SpaceFn       |Caps |SYS |  |Lef|Dow|Rig|
      * `---------------------------------------------------------------'
      */
     [L_MAIN] = KEYMAP( \
-	       KC_GESC,      KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,   KC_MINS,    KC_EQL,   KC_BSLS,    KC_GRV,TG(L_GAME), \
+           KC_GESC,      KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,   KC_MINS,    KC_EQL,   KC_BSLS,    KC_GRV,TG(L_GAME), \
             KC_TAB,      KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,   KC_LBRC,   KC_RBRC,   KC_BSPC,               KC_DEL, \
           MT_CTMUX,      KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,   KC_SCLN,   KC_QUOT,   KC_NUHS,               KC_ENT,   KC_PGUP, \
            KC_LSPO,   KC_NUBS,      KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,              KC_RSPC,     KC_UP,   KC_PGDN, \
-		 MO(L_FCT),   KC_LALT,   KC_LGUI,                                                           LT(L_FCT, KC_SPC),   KC_CAPS, MO(L_ADV),    XXXXXX,   KC_LEFT,   KC_DOWN,   KC_RGHT  \
+        OSL(L_OSM),   KC_LALT,   KC_LGUI,                                                           LT(L_SFN, KC_SPC),   KC_CAPS, MO(L_SYS),    XXXXXX,   KC_LEFT,   KC_DOWN,   KC_RGHT  \
     ),
-    /* Layer 1: Common Fonctions Layer
+    /* SpaceFn Layer
      * ,---------------------------------------------------------------.
      * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |   |   |
      * |---------------------------------------------------------------|
@@ -58,34 +60,42 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |                  |    |    |    |  |   |   |   |
      * `---------------------------------------------------------------'
      */
-    [L_FCT] = KEYMAP( \
-            ______,     KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,    KC_F10,    KC_F11,    KC_F12,    ______,    ______,    ______, \
-            ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,               ______, \
-            ______,    ______,    ______,    ______,    ______,   KC_HOME,   KC_LEFT,   KC_DOWN,     KC_UP,  KC_RIGHT,    KC_END,    ______,    ______,               ______,    ______, \
-       TD(KC_BCBO),    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,          TD(KC_BCBC),    ______,    ______, \
-            ______,    ______,    ______,                                                                      ______,    ______,    ______,    ______,    ______,    ______,    ______  \
+    [L_SFN] = KEYMAP( \
+            XXXXXX,     KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,    KC_F10,    KC_F11,    KC_F12,    XXXXXX,    XXXXXX,    XXXXXX, \
+            XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,               XXXXXX, \
+            XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,   KC_HOME,   KC_LEFT,   KC_DOWN,     KC_UP,  KC_RIGHT,    KC_END,    XXXXXX,    XXXXXX,               XXXXXX,    XXXXXX, \
+       TD(KC_BCBO),    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,          TD(KC_BCBC),    XXXXXX,    XXXXXX, \
+            XXXXXX,    XXXXXX,    XXXXXX,                                                                      ______,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX  \
     ),
-    /* Layer 2: Advanced System Layer
+    /* System Layer
      * ,----------------------------------------------------------------.
      * |EJT|   |   |   |   |   |   |   |   |   |   |   |   |   |   |    |
      * |----------------------------------------------------------------|
-     * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |Mute|
+     * |     |   |   |   |   |   |   |   |   |   |Pau|   |   |P/Pau|Mute|
      * |----------------------------------------------------------------|
      * |      |   |   |   |   |   |   |   |   |   |   |   |        |Vol+|
      * |----------------------------------------------------------------|
-     * |        |   |   |   |   |   |   |   |   |   |   |      | N^|Vol-|
+     * |        |   |   |Clc|   |   |   |   |   |   |   |      | N^|Vol-|
      * |----------------------------------------------------------------|
      * |    |    |    |                  |    |    |    |  | N<| Nv|N>  |
      * `----------------------------------------------------------------'
      */
-    [L_ADV] = KEYMAP( \
-           KC_EJCT,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______, \
-            ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,              KC_MUTE, \
-            ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,               ______,   KC_VOLU, \
-            ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,    ______,               ______, CA(KC_UP),   KC_VOLD, \
-            ______,    ______,    ______,                                                                      ______,    ______,    ______,    ______,CA(KC_LEFT),CA(KC_DOWN),CA(KC_RGHT) \
+    [L_SYS] = KEYMAP( \
+           KC_EJCT,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX, \
+            XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,   KC_PAUS,    XXXXXX,    XXXXXX,   KC_MPLY,              KC_MUTE, \
+            XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,               XXXXXX,   KC_VOLU, \
+            XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,   KC_CALC,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,               XXXXXX, CA(KC_UP),   KC_VOLD, \
+            XXXXXX,    XXXXXX,    XXXXXX,                                                                      XXXXXX,    XXXXXX,    ______,    XXXXXX,CA(KC_LEFT),CA(KC_DOWN),CA(KC_RGHT) \
     ),
-    /* Layer 3: Gaming Layer */
+    /* One-Shot Macros Layer */
+    [L_OSM] = KEYMAP( \
+    CA(KC_DEL),    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX, \
+    CS(KC_ESC),    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,               XXXXXX, \
+        XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,               XXXXXX,    XXXXXX, \
+        XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,               XXXXXX,    XXXXXX,    XXXXXX, \
+        ______,    XXXXXX,    XXXXXX,                                                                      XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX  \
+        ),
+    /* Gaming Layer */
     [L_GAME] = KEYMAP( \
             KC_ESC,      KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,   KC_MINS,    KC_EQL,   KC_BSLS,    KC_GRV,    ______, \
             KC_TAB,      KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,   KC_LBRC,   KC_RBRC,   KC_BSPC,               KC_DEL, \
@@ -97,16 +107,16 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-	switch(id) {
-	case M_TMUX:
-		return MACRO_TAP_HOLD_MOD(record, MACRO(D(LCTL), T(B), U(LCTL), END), LCTL);
-	}
-	return MACRO_NONE;
+    switch(id) {
+    case M_TMUX:
+        return MACRO_TAP_HOLD_MOD(record, MACRO(D(LCTL), T(B), U(LCTL), END), LCTL);
+    }
+    return MACRO_NONE;
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-	[KC_BCBO] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LCBR),
-	[KC_BCBC] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR)
+    [KC_BCBO] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LCBR),
+    [KC_BCBC] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR)
 };
 
 /* const uint16_t fn_actions[] = { */
@@ -114,10 +124,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 /* void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) */
 /* { */
-/* 	if (id == K_ESC) { */
-/* 		void (*method)(uint8_t) = (record->event.pressed) ? &register_code : &unregister_code; */
-/* 		uint8_t shifted = get_mods() & (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)); */
+/*  if (id == K_ESC) { */
+/*      void (*method)(uint8_t) = (record->event.pressed) ? &register_code : &unregister_code; */
+/*      uint8_t shifted = get_mods() & (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)); */
 
-/* 		method(shifted ? KC_GRAVE : KC_ESCAPE); */
-/* 	} */
+/*      method(shifted ? KC_GRAVE : KC_ESCAPE); */
+/*  } */
 /* } */
